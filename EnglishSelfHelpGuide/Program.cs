10b4e71b3bde20bc4df0edbed3577c5a65c262e9
mixail <v1tus5555@yandex.by>
2025-09-26 16:53:35 +0300

    дороботка program.cs

--- EnglishSelfHelpGuide/Program.cs
+++ EnglishSelfHelpGuide/Program.cs
@@ -7,7 +7,6 @@ using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddDbContextPool<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection")));
@@ -17,11 +16,11 @@ builder.Services.AddScoped<IIrregularVerbService, IrregularVerbService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+   
     app.UseHsts();
 }
 
